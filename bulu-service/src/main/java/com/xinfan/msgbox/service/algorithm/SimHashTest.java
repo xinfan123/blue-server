ef347ca645fe1bfb67e6a3640af242f95099c707
sodoa <165614968@qq.com>
2014-11-14 21:55:46 +0800

    增加平台对外接口包

--- bulu-service/src/main/java/com/xinfan/msgbox/service/algorithm/SimHashTest.java
+++ bulu-service/src/main/java/com/xinfan/msgbox/service/algorithm/SimHashTest.java
@@ -10,8 +10,9 @@ public class SimHashTest {
 		long start  = System.currentTimeMillis();
 		
 		for(int i=0;i<1;i++){
-			String word1 = "本的长期寻长有想法的伙伴一起共事";
-			String word2 = "求有想法的伙伴一起创业";
+			String word1 = "本的长期寻长有想法的伙伴一起共事本的长期寻长有想法的伙伴一起共事";
+			String word2 = "求有想法的伙伴一起创业本的长期寻长有想法的伙伴一起共事s/ss" +
+					"";
 			double f = SimHash.getSimilarity(word1, word2);
 			System.out.println(f);
 		}
